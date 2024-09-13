3adc9a84e177480a4b20f07de46cb7544899fbd0
Jewelryxx <Supapit9177@gmail.com>
2024-09-13 20:13:26 +0700

    first commit

---
+++ Ep6/index.ts
@@ -0,0 +1,59 @@
+// const score:number[] = [10,23,26,30,33]
+
+// let sum:number = 0
+// for (let i = 0; i < score.length; i++) {
+//     sum = sum + score[i] หาผลรวม
+// }
+// const avg = sum / score.length หาค่าเฉลี่ย
+// console.log(sum)
+// console.log(avg)
+
+// const sName: string = 'n1, n2, n3, n4, n5, n6'
+
+// const sNameArray = sName.split(',')
+// console.log(sNameArray.join('#'))
+
+// const Aname: string = "หมา*แมว*ช้าง*เสือ"
+
+// const AnameArray: string[] = Aname.split('*')
+// console.log(AnameArray)
+
+// const Aname: string = "หมา*แมว*ช้าง*เสือ"
+
+// const AnameArray: string[] = Aname.split('*')
+// AnameArray.pop()// ดึงตัวสุดท้ายออก
+// AnameArray.push('กระต่าย')//ใส่คำท้ายใหม่
+// AnameArray.shift()// ดึงตัวหน้าออก
+// AnameArray.unshift('ยีราฟ')// ใส่ของเข้าไปในตัวหน้า
+// //console.log(AnameArray.sort())//เรียงลำดับขั้น a-z ก-ฮ
+// //console.log(AnameArray.reverse())//เรียงกลับ z-a ฮ-ก
+// console.log(AnameArray.slice(0,2))// เอาคำออกมาตามลำดับที่กำหนด
+
+// const fruits = ["Banana","Orange","Apple","Mango"]
+// console.log(fruits.sort())
+// console.log(fruits.reverse())
+//JS - Javascript object
+// const me = {
+//     fristName: "siriluk",
+//     lastName: "poangsamlee",
+//     age: 21
+// }
+// console.log(me)
+// ประวัติ
+// ชื่อ: "siriluk",
+// นามสกุล: "poangsamlee",
+// อายุ: 21
+// การบ้าน
+const perSoNalInForMaTion = {
+    fristname:"siriluk",
+    currentaddress: "5 nunglinjee 17",
+    subdistrict: "narratives 10",
+    street: "nunglinjee",
+    districk: "narativas 10",
+    province:"Sarthon",
+    postalcode: 10120,
+    tel: "0255664488",
+    numberphone: "0947963100",
+    email: "siriluk.gmail.com",
+    }
+console.log(perSoNalInForMaTion)
